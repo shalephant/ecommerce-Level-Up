9bf7b28d9ec876c9514f7843e1f8281b4c252cbc
shalephant <shakotsiklauri@gmail.com>
2023-04-28 00:52:28 +0400

    add purchase function and list and modify existing one

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
+
 class Ecommerce:
+
     def __init__(self):
         self.products = {}
+        self.purchases = []
 
+    """
+        save_product function adds a new product to the catalog or updates existing products price and name
+    """
     def save_product(self, product_id, product_name, product_price):
         if product_id in self.products:
             self.products[product_id]['name'] = product_name
             self.products[product_id]['price'] = product_price
             print(f"Product with id {product_id} was updated")
         else:
-            self.products[product_id] = product_id
-            self.products[product_id] = {'name': product_name}
-            self.products[product_id] = {'price': product_price}
+            self.products[product_id] = {'name': product_name, 'price': product_price, 'quantity': 0}
             print(f"Product with id {product_id} was saved")
+
+    """
+            purchase_product function purchases an existing product, increasing the product quantity in products
+            catalog and adding quantity and price to the purchases list for the history
+    """
+    def purchase_product(self, product_id, quantity, price):
+        if product_id not in self.products:
+            print(f"No product with id {product_id} found")
+        else:
+            self.purchases.append((quantity, price))
+            self.products[product_id]['quantity'] += int(quantity)
+            print(f"{quantity} units of product with id {product_id} was purchased for {price} a unit")
+
 
 
 if __name__ == '__main__':
@@ -22,6 +39,8 @@ if __name__ == '__main__':
         part = command.split()
         if part[0] == "save_product":
             ecom.save_product(part[1], part[2], float(part[3]))
+        elif part[0] == "purchase_product":
+            ecom.purchase_product(part[1], int(part[2]), float(part[3]))
         elif part[0] == "exit":
             break
         else:
